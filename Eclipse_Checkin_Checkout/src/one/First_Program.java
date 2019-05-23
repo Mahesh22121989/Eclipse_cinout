26f3004f0b8492722484e8e3b8452e5073b46b19
Mahesh Babu <maheshit22@gmail.com>
2019-05-23 23:17:10 +0530

    New package and class was created.

---
+++ Eclipse_Checkin_Checkout/src/one/First_Program.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package one;
+
+/**
+ * @author acer
+ *
+ */
+public class First_Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
